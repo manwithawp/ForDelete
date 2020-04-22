ee25df9eea8d8929aa4535c3d288dd8f8f38908d
YuraForward <iury.fedoroff2018@yandex.ru>
2020-04-22 10:14:26 +0300

    "branch 4 first commit"

--- src/Main.java
+++ src/Main.java
@@ -14,6 +14,7 @@ public class Main {
 
         System.out.println("main first commit");
         System.out.println("main first commit");
+        System.out.println("branch 4 first commit");
 
     }
 }
