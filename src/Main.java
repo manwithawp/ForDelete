f07c864cccd9453bff46eb7078d1d286392c548d
YuraForward <iury.fedoroff2018@yandex.ru>
2020-04-22 17:42:19 +0300

    8

--- src/Main.java
+++ src/Main.java
@@ -14,8 +14,7 @@ public class Main {
 
         System.out.println("main first commit");
         System.out.println("main first commit");
-        System.out.println("brangfhch 7");
-        System.out.println("7");
+        System.out.println("8");
 
     }
 }
